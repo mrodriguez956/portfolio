0135530c78c807e073033087b4ad0c46292e16d1
mrodriguez956 <164256777+mrodriguez956@users.noreply.github.com>
2025-10-30 16:51:41 -0500

    updated script.ts

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,1 +1,21 @@
-document.querySelector("");
+document.addEventListener("DOMContentLoaded", () => {
+  const projectTapes = document.querySelectorAll(".tape");
+  const projectPreview = document.querySelector(
+    "#project-preview",
+  ) as HTMLImageElement;
+
+  if (projectTapes?.length > 0 && projectPreview) {
+    projectTapes.forEach((tape) => {
+      tape.addEventListener("click", (e) => {
+        const clickedTape = e.currentTarget as HTMLImageElement;
+        const previewSrc = clickedTape.dataset.preview;
+
+        console.log("clicked " + clickedTape);
+
+        if (previewSrc) {
+          projectPreview.src = previewSrc;
+        }
+      });
+    });
+  }
+});
